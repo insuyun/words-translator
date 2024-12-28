d03ae712a8097cfd3853f5a1f948c5527559207d
Insu Yun <wuninsu@gmail.com>
2024-12-28 22:16:25 +0900

    feat: initial version of words translator

---
+++ README.md
@@ -0,0 +1,28 @@
+# Words Translator: 설교문 번역기
+
+## 세팅
+1. `openai` API 키를 발급받아 `.env` 파일에 저장합니다.
+```bash
+# .env
+OPENAI_API_KEY=sk-...
+```
+
+2. `requirements.txt`에 있는 패키지를 설치합니다.
+```bash
+pip install -r requirements.txt
+```
+
+## 사용법
+1. [네이버 클로바 노트](https://clova.ai/note)를 이용하여 설교문을 녹음하여 텍스트로 변환합니다.
+2. 녹음 텍스트를 복사하여 `samples/1_raw.txt`와 같은 파일에 저장합니다.
+3. 해당 파일을 확인하여 "------"로 문단을 구분하여 `samples/2_break.txt`와 같은 파일에 저장합니다.
+4. `words.py`를 실행하여 맞춤법 검사를 진행합니다
+```bash
+python words.py --mode proofread --input samples/2_break.txt --output samples/3_proofread.txt
+
+```
+5. 맞춤법 검사 결과를 확인하여 오류가 있는 부분을 수정합니다.
+6. `words.py`를 실행하여 번역을 진행합니다
+```bash
+python words.py --mode translate --input samples/3_proofread.txt --output samples/4_translated.txt
+```
---
+++ words.py
@@ -0,0 +1,89 @@
+
+import re
+import argparse
+import openai
+import tiktoken
+from typing import Literal
+from pathlib import Path
+from dotenv import load_dotenv
+
+GPT_TOKEN_LIMIT = 8192
+Mode = Literal["proofread", "translate"]
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=str, choices=["proofread", "translate"])
+    parser.add_argument("input", type=str)
+    parser.add_argument("output", type=str)
+    return parser.parse_args()
+
+def count_tokens(text: str) -> int:
+    # GPT-3.5-turbo 인코딩 사용
+    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+    return len(encoding.encode(text))
+
+def _check_token_limit(paragraphs: list[str]):
+    for paragraph in paragraphs:
+        tokens = count_tokens(paragraph)
+        if tokens > GPT_TOKEN_LIMIT:
+            raise ValueError(f"Total tokens exceed the limit: {tokens}", paragraph)
+
+def _make_prompt(paragraph: str, mode: Mode) -> str:
+    if mode == "proofread":
+        return f"""문장을 검수하고 오류를 수정하여 반환해주세요. 성경 문장은 개역개정판을 기준으로 합니다. 문장의 내용은 바꾸지 말고 내용만 수정해주세요. 입력드리는 내용을 하나의 문단으로 처리해주세요. 즉 줄바꿈 없이 하나의 문단으로 처리해주세요. 결과만 반환하고 설명은 하지 않습니다.
+
+        {paragraph}
+        """
+    elif mode == "translate":
+        return f"""문장을 영어로 번역해주세요. 성경 문장은 NIV를 기준으로 합니다. 문장의 내용은 바꾸지 말고 내용만 번역해주세요. 입력드리는 내용을 하나의 문단으로 처리해주세요. 즉 줄바꿈 없이 하나의 문단으로 처리해주세요. 결과만 반환하고 설명은 하지 않습니다.
+
+        {paragraph}
+        """
+
+def _make_system_prompt(mode: Mode) -> str:
+    if mode == "proofread":
+        return "당신은 문장을 검수하는 프로그램입니다. 문장을 검수하고 오류를 수정하여 반환해주세요."
+    elif mode == "translate":
+        return "당신은 문장을 번역하는 프로그램입니다. 문장을 번역하여 반환해주세요."
+
+def _to_paragraphs(mode: Mode, text: str) -> list[str]:
+    if mode == "proofread":
+        return re.split(r"-{5,}", text)
+    elif mode == "translate":
+        return re.split(r"\n\n", text)
+
+def main():
+    args = parse_args()
+
+    if Path(args.output).exists():
+        print(f"[!] Output file already exists: {args.output}")
+        return
+
+    input = Path(args.input).read_text()
+    paragraphs = _to_paragraphs(args.mode, input)
+
+    _check_token_limit(paragraphs)
+    output = ""
+    for paragraph in paragraphs:
+        prompt = _make_prompt(paragraph, args.mode)
+        response = openai.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": _make_system_prompt(args.mode)},
+                {"role": "user", "content": prompt},
+            ],
+        )
+
+        if paragraph.strip() == "":
+            continue
+
+        print(f"FROM: {paragraph}")
+        print(f"TO: {response.choices[0].message.content}\n\n")
+        output += response.choices[0].message.content + "\n\n"
+
+    Path(args.output).write_text(output)
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
